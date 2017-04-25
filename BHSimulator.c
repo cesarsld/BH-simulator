11c8c830cd4c14c236d7538da0d04367e5c4a6bd
cesarsld <jaimes.cesar88@gmail.com>
2017-04-25 21:53:30 +0100

    V5
    Starting to add Boss code to make dummy fight back and actually have a
    fight

--- BHSimulator.c
+++ BHSimulator.c
@@ -25,12 +25,11 @@ typedef struct {
 Hero hero[5];           // size of array indicated max amount of heroes in team.
 
 //int a,b;
-int power, stamina, agility, hp;
+int dummypower, dummystamina, dummyagility, hpdummy=100000;
 int critChance, critDamage, block, evade, deflect, DS, runePower, runeAgi;
 int counter;
-int hpdummy=100000;
-int countermax=100;
-float tr=2, interval, privatecounter=0;
+int countermax = 100;
+float tr = 2, interval, dummycounter=0;
 int cycle, attacktimer, j=0, attacksdone=0;
 int playerNo;
 int i;
@@ -40,8 +39,8 @@ int i;
 
 float turnRate(int b, int a){
     tr = ((a + b)/2);
-    tr= pow(tr, 2);
-    tr=tr/(100*b);
+    tr = pow(tr, 2);
+    tr = tr/(100*b);
     return tr;
 }
 
@@ -54,17 +53,34 @@ void updateTimer(){
 //}
 
 int main() {
-    printf("How many heroes will you use?\n");
-    scanf("%d", &playerNo);
+   // printf("How many heroes will you use?\n");
+   // scanf("%d", &playerNo);
+    hero[0].power = 500;
+    hero[1].power = 500;
+    hero[2].power = 500;
+    hero[3].power = 500;
+    hero[4].power = 500;
+    hero[0].stamina = 200;
+    hero[1].stamina = 200;
+    hero[2].stamina = 200;
+    hero[3].stamina = 200;
+    hero[4].stamina = 200;
+    hero[0].agility = 500;
+    hero[1].agility = 500;
+    hero[2].agility = 500;
+    hero[3].agility = 500;
+    hero[4].agility = 500;
+    
+
     for(i=0 ; i<playerNo ; i++){
-        printf("How much power on hero %d ?\n", i+1);
+        /*printf("How much power on hero %d ?\n", i+1);
         scanf("%d", &hero[i].power);
         printf("How much stamina on hero %d ?\n", i+1);
         scanf("%d", &hero[i].stamina);
         printf("How much agility on hero %d ?\n", i+1);
-        scanf("%d", &hero[i].agility);
+        scanf("%d", &hero[i].agility);*/
         
-        hero[i].hp = hero[i].stamina * 10;
+        hero[i].hp = hero[i].stamina * 101;
         hero[i].tr = turnRate(hero[i].power, hero[i].agility);
         hero[i].interval = countermax / hero[i].tr;
         hero[i].counter=0;
@@ -73,18 +89,21 @@ int main() {
     
     while (hpdummy>0){
         for(cycle=1 ; cycle<=countermax ; cycle++){
-            privatecounter++;
+            dummycounter++;
             for(i=0 ; i<playerNo ; i++){
                 hero[i].counter++;
                 if(hero[i].counter >= hero[i].interval && hero[i].hp > 0){
                     hpdummy -= hero[i].power;
                     hero[i].counter -= hero[i].interval;
                     attacksdone++;
-                    if (hpdummy>=0){
-                        break;
+                    if (hpdummy<=0){
+                        printf("You killed the dummy!\n");
+                        i = playerNo;
+                        cycle = countermax;
                     }
                 }
             }
+            if (hpdummy > 0  ){}
         }
     }
     printf("hits done: %d\n", attacksdone);
