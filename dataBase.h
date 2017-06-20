81e8b1488512417c6cea37a0775b6111e7db1741
cesarsld <jaimes.cesar88@gmail.com>
2017-06-20 19:51:21 +0100

    V2.5

--- dataBase.h
+++ dataBase.h
@@ -1,7 +1,7 @@
 //  BH Simulator
 //
-//  Created by Cesar Salad Jaimes on 01/06/2017.
-//  Copyright © 2017 Cesar Salad Jaimes. All rights reserved.
+//  Created by Shadown88 on 01/06/2017.
+//  Copyright © 2017 Shadown88. All rights reserved.
 //
 #include <stdbool.h>
 #pragma once
